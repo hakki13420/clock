bda774936416b7cb6b9bdf10e1a419bc8b6ab51b
hakki <hak_rab@yahoo.fr>
2025-01-24 10:07:59 +0100

    errors correction

--- main.js
+++ main.js
@@ -13,10 +13,14 @@ setInterval(() => {
     const minutes=date.getMinutes()
     const seconds=date.getSeconds()
 
-    hoursElement.style.transform=`rotate(${hours*30}deg)`
-    minutesElement.style.transform=`rotate(${(minutes*5)+(seconds/60)}deg)`
+    hoursElement.style.transform=`rotate(${hours*30+Math.round((minutes/60)*30)}deg)`
+    minutesElement.style.transform=`rotate(${(minutes*6)+Math.floor((seconds/60)*6)}deg)`
     secondsElement.style.transform=`rotate(${seconds*6}deg)`
 
-    digital.innerHTML=`${hours}:${minutes}:${seconds}`
+    console.log("deg hoursc",Math.round((minutes/60)*30))
+    console.log("deg min",Math.floor((seconds/60)*6))
+
+
+    digital.innerHTML=`${("0"+hours).slice(-2)}:${("0"+minutes).slice(-2)}:${("0"+seconds).slice(-2)}`
 
 },1000);
